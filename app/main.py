605ad32884cd3dc03094eb9241719cdb09e0d825
grekiki <gregor1234567890@gmail.com>
2020-10-11 19:47:48 +0200

    add caching of site visits

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 from model import Model
+import random
 
 app = Flask(__name__)
 m = Model()
 
 @app.route('/main')
 def hello_world():
-    return m.output
+    ip = request.access_route[0]
+    print(ip)
+    m.update(str(ip))
+    return m.get_output()
--- app/model.py
+++ app/model.py
@@ -1,4 +1,27 @@
+import os
+
 class Model:
     def __init__(self):
-        self.output = "Hello world!"
-    
+        pass
+
+    def update(self, s):
+        if os.path.exists("cache/" + s + ".txt"):
+            curr = int(open("cache/" + s + ".txt","r").read())
+
+            with open("cache/" + s + ".txt","w") as f:
+                f.write(str(curr+1) + "\n")
+
+        else:
+            with open("cache/" + s + ".txt","w") as f:
+                f.write("100\n")
+
+    def get_output(self):
+        output = ""
+        try:
+            for f in os.listdir("cache"):
+                print(f)
+                output += f + "<br>"
+            output += "Hello world!"
+        except Exception:
+            return "Cache folder issues"
+        return output
